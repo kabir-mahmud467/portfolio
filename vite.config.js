c0c36839c8a75455196717fb13c25079b554ebdc
Kabir Mahmud <kabir120706@gmail.com>
2026-01-19 07:36:31 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Change 'portfolio' to your actual repository name
-  base: '/portfolio/', 
+  base: '/',
   build: {
     outDir: 'dist',
   },
